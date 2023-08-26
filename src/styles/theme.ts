10a6673ee4827fac3d1bd13497e8aa3997802358
Ceagah2 <carlosceagah@gmail.com>
2023-08-26 10:33:50 -0300

    style: create theme file, and rename index.css to global.css

---
+++ src/styles/theme.ts
@@ -0,0 +1,34 @@
+export const theme = {
+  colors: {
+    background: '#DEF2C8',
+    lightBlue: '#407899',
+    red: '#AF1B3F',
+    yellow: '#DE9E36',
+    green: '#3F612D',
+
+    warning: '#FFA500',
+    error: '#FF0000',
+    success: '#00FF00',
+    lightGray: '#D3D3D3',
+  },
+  fonts: {
+    heading: 'Noto Sans Chorasmian',
+    body: 'Poppins',
+  },
+  fontSizes: {
+    heading: '2rem',
+    subHeading: '1.5rem',
+    body: '1rem',
+    description: '0.8rem',
+    small: '0.7rem',
+  },
+  fontWeights: {
+    thin: 100,
+    extraLight: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+  },
+};
